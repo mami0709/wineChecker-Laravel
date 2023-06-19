410cb42c121fd131a93da80ecf3bf471d625d4e8
mami0709 <mmmxxxzelda@yahoo.co.jp>
2023-06-19 12:38:48 +0900

    CORSエラーが出ないように

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResultAkaController;
+use App\Http\Controllers\ResultShiroWineController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/resultAkaWine', [ResultAkaController::class, 'index']);
+Route::get('/resultShiroWine', [ResultShiroWineController::class, 'index']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
